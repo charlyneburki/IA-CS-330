6719743ba0bca1abedb347fa9b28174ab564bad6
Charlyne Burki <charlyne@charlynes-macbook-pro.home>
2020-05-04 18:51:04 +0200

    class traitant les donnes

---
+++ donnees/traitement.py
@@ -0,0 +1,22 @@
+
+class TraitementDonnees:
+    """ Traite les données fournies. """
+
+    def import_donnees(self, filepath):
+        """ Importe les données à partir du filepath """
+    #impore les données sous forme de dictionnaire
+        from csv import DictReader
+        # open file in read mode
+        with open(filepath, 'r', encoding='UTF-8-sig') as read_obj:
+            # pass the file object to DictReader() to get the DictReader object
+            dict_reader = DictReader(read_obj)
+            # get a list of dictionaries from dct_reader
+            donnees = list(dict_reader)
+            # print list of dict i.e. rows
+            final_donnees = []
+            for donnee in donnees:
+                    sample = [donnee['target']]
+                    del donnee['target']
+                    sample.append(donnee)
+                    final_donnees.append(sample)
+        return final_donnees
