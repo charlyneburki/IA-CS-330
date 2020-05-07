bed58e5eb8e9ed83335eb550265431fd3969cd84
Charlyne Burki <charlyne@charlynes-macbook-pro.home>
2020-05-07 23:02:11 +0200

    début partie 3

--- projet.py
+++ projet.py
@@ -5,7 +5,7 @@ from result_values import ResultValues
 results = ResultValues()
 
 print('arbre resultant:')
-print(results.arbre)
+#print(results.arbre)
 
 print('pourcentage de classifications correcte:')
 print(results.evaluer_model())
@@ -15,5 +15,7 @@ print('Exemplification :')
 for i in range(len(results.donnees_test)):
     print(results.arbre.classifie(results.donnees_test[i]))
 
-
+#trouver les règles à partir de l'arbre établi. 
+#regles = results.arbre.gen_regles()
+#print(len(regles))
 
