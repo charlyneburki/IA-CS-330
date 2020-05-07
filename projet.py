90230d33b67779fc2583880868182b38bcbea181
Charlyne Burki <charlyne@charlynes-macbook-pro.home>
2020-05-07 21:27:12 +0200

    modification de la structure

--- projet.py
+++ projet.py
@@ -1,43 +1,19 @@
-from moteur_id3.noeud_de_decision import NoeudDeDecision
-from moteur_id3.id3 import ID3
-from donnees.traitement import TraitementDonnees
-from donnees.statistiques import StatistiquesID3
+from result_values import ResultValues
 
-#partie 1
 
-importation = TraitementDonnees()
-donnees_entrainement = importation.import_donnees('res/train_bin.csv')
 
-id3 = ID3()
-arbre = id3.construit_arbre(donnees_entrainement)
-print('Arbre de décision :')
-print(arbre)
+results = ResultValues()
 
-#calculs à faire pour partie 1
-stat = StatistiquesID3()
-#trouve le nombre d'enfants de l'arbre
-#et autres stat A COMPLETER
-nb_enfants = len(arbre.enfants)
+print('arbre resultant:')
+print(results.arbre)
 
-#partie 2
+print('pourcentage de classifications correcte:')
+print(results.evaluer_model())
 
-donnees_test = importation.import_donnees_test('res/test_public_bin.csv')
-
-#resultats de la classification
-predicted = []
-actual = []
 
 print('Exemplification :')
+for i in range(len(results.donnees_test)):
+    print(results.arbre.classifie(results.donnees_test[i]))
 
-for donnee in donnees_test:
-    rep = arbre.classifie(donnee)
-    actual.append(donnee['target'])
-    #print(rep)
-    #takes the last character of rep i.e the result of classification
-    predicted.append(rep[-1])
 
-print('pourcentage de classifications correctes :')
-print(stat.evaluer_similitude(predicted,actual))
-
-#partie 3
 
