9288694afdc28b855ef7cc2a0d398d49f26c423b
Charlyne Burki <charlyne@charlynes-macbook-pro.home>
2020-05-09 18:47:45 +0200

    FULLY WORKING SORTING ALGO PART 3

--- result_values.py
+++ result_values.py
@@ -21,34 +21,24 @@ class ResultValues():
         
         # Task 3
             #chose a random example to see how it works
-        self.faits_initiaux = self.donnees_test[0]
-        self.regles = self.arbre.regles_generation()
-        #self.justification_prediction(self.faits_initiaux)
+        self.faits_initiaux = self.donnees_test[0] #what is this ?
+        self.regles = self.arbre.generer_regles()
+        
         # Task 5
         self.arbre_advance = None
 
     def get_results(self):
         return [self.arbre, self.faits_initiaux, self.regles, self.arbre_advance]
     
-    def get_arbre(self):
-        return self.arbre
-    
-    def get_faits_initiaux(self):
-        return self.faits_initiaux
-    
-    def get_regles(self):
-        return self.regles
-    
-    def get_arbre_avance(self):
-        return self.regles
-    
     def import_data(self):
+        """se charge d'importer les données pour le programme """
         importation = TraitementDonnees()
         donnees_entrainement = importation.import_donnees('res/train_bin.csv')
         donnees_test = importation.import_donnees_test('res/test_public_bin.csv')
         return donnees_entrainement, donnees_test
     
     def evaluer_model(self):
+        """ evalue le modèle basé sur les données de test. Retourne le pourcentage d'évaluation correcte """
         predicted = []
         actual = []
         
@@ -59,69 +49,80 @@ class ResultValues():
             #takes the last character of rep i.e the result of classification
             predicted.append(rep[-1])
         
-        return self.stat.evaluer_similitude(predicted,actual)
-        
-    def justification_prediction(self, exemple):
-        """ finds the justifications for classifying an example """
-        #sort example keys alphabetically
-        sorted_example =sorted(exemple.items())
-        #sort rules by keys alphabetically
-        sorted_rules = self.sort_rules()
-        
-        #sorted_example = exemple
-        #sorted_rules = self.regles
-        
-        justification = []
-        minimum_correct = 0
-            
-        for rule in sorted_rules:
-            correct_cond = 0
-            for cond_rule,cond_ex in zip(rule,sorted_example):
-                if cond_rule == cond_ex:
-                    correct_cond += 1
-                if correct_cond > minimum_correct:
-                    minimum_correct = correct_cond
-                    justification.append(rule)
-        
-        #we choose to return the rule that has the most conditions true
-        return_value = max(justification, key=len)
-        #return_value = justification[0]
-
-        return return_value
-    
-    def sort_rules(self):
-        """ orders the rules in alphabetical order """
-        sorted_rules = []
-        for regle in self.regles:
-        
-            sorted_rule= sorted(regle, key=lambda r: r[0])
-            
-            result = sorted_rule.pop(0)
-            sorted_rule2= sorted(sorted_rule, key=lambda r: r[0])
-            sorted_rule2.append(result)
-            
-            sorted_rules.append(sorted_rule2)
-        return sorted_rules
-        
-    def justification_prediction2(self, patient):
-        
-        best_rule = None
-        best = 0
-        best_overall = -1
-        
-        for rule in self.regles:
-            best = 0
-            for parameter in patient:
-                for conditions_rule in rule:
-                    if (conditions_rule[0] == parameter) and (conditions_rule[1] == patient[parameter]):
-                        #print(conditions_rule)
-                        #print(parameter)
-                        #print(patient[parameter])
-                        best += 1
-            if best > best_overall:
-                best_overall = best
-                best_rule = rule
+        return self.stat.evaluer_similitude(predicted,actual)*100
                 
-        return best_rule
-    
+    def determine_equality(self,patient, regle):
+        """ determine si une regle correspond aux conditions du patient en comptabilisant le nombre de conditions vraies de la règle pour le patient"""
+        sorted_rules = []
+        rule = regle.copy()
+        #we sort the rule to make it easier to classify
+        result = rule.pop()
+        sorted_rule= sorted(rule, key=lambda r: r[0])
+        sorted_rule.append(result)
+        
+        sorted_rules.append(sorted_rule)
+        
+        #we sort the example alphabetically as well
+        sorted_patient= sorted(patient.items())
+        
+        total = 0
+        for cond_rule in sorted_rule:
+            for cond_patient in sorted_patient:
+                if cond_rule==cond_patient:
+                    total += 1
 
+        if total == (len(sorted_rule)-1):
+            return True
+        else:
+            return False
+    
+    def justification_prediction(self, patient):
+        """ recherche la règle correspondant aux conditions du patients. Retourne la meilleure règle qui décrit ses symptotes. """
+        best_rule = []
+        for rule in self.regles:
+            equality = self.determine_equality(patient, rule)
+            
+            if equality:
+                return rule
+        
+        
+        #return never used
+        #TO DO : FIND BETTER ALTERNATIVE THAN RETURNING FIRST RULE
+        print('no suitable rule found')
+        return self.regles[0]
+    
+    def evaluer_regles(self):
+        
+        predicted = []
+        actual = []
+        
+        for donnee in self.donnees_test:
+            rep = self.arbre.classifie(donnee)
+            actual.append(donnee['target'])
+            
+            #takes the last character of rep i.e the result of classification
+            result = self.justification_prediction(donnee)
+            predicted.append(result[-1][-1])
+        
+        return self.stat.evaluer_similitude(predicted,actual)*100
+        
+    def rprs_justification(self, patient):
+        
+        justification = self.justification_prediction(patient)
+        print('Patient avec :')
+        for key,value in patient.items():
+            if key == 'target':
+                print('devrait être {}.'.format(value))
+            else:
+                print('{} = {},'.format(key,value))
+                
+        print('est classifié comme {}'.format(justification[-1][-1]))
+        print('car :')
+        for condition in justification:
+            print('{} = {},'.format(condition[0],condition[1]), end=' ')
+        
+            
+        
+        
+                
+                
