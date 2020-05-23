0ea74bb05a51d6f110362421d108f7e02aa300a9
Charlyne Burki <charlyne@charlynes-macbook-pro.home>
2020-05-23 13:42:45 +0200

    new project template

--- projet.py
+++ projet.py
@@ -2,43 +2,50 @@ from result_values import ResultValues
 
 results = ResultValues()
 
+arbre, faits_initiaux, regles, arbre_advance =results.get_results()
+print('TACHE #1:')
 print('arbre resultant:')
-#print(results.arbre)
+#print(arbre)
 
+print('=-=-=-=-=-=-=-=-=-=-=-=-=')
+print('TACHE #2:')
 print('pourcentage de classifications correcte:')
-print(results.evaluer_model())
+print(results.evaluation_model_1)
 
 
-print('Exemplification :')
-#for i in range(len(results.faits_initiaux)):
-#   print(results.arbre.classifie(results.faits_initiaux[i]))
+#print('Exemplification :')
+#for i in range(len(results.donnees_test)):
+#   print(results.arbre.classifie(results.donnees_test[i]))
 
-print('****')
-print('part 3')
-print('Affichage des règles trouvées pour le modèle:')
-regles = results.regles
+print('=-=-=-=-=-=-=-=-=-=-=-=-=')
+print('TACHE #3:')
 print('Il y a ' + str(len(regles)) + ' règles générées.')
 for regle in regles:
     print (regle)
 
-    
-
-#checks the % correctly classified
-print('pourcentage de classification correcte basé sur les règles:')
-print(results.evaluer_regles())
-
+print('=-=-=-=-=-=-=-=-=-=-=-=-=')
+print('TACHE #4:')
 #affiche en bon format la prédiction d'un patient
-results.rprs_justification(results.faits_initiaux[7])
+print('exemplification d un patient diagnosé')
+#results.rprs_justification(results.donnees_test[7])
 
-for patient in results.faits_initiaux:
-    results.diagnose_patient(patient)
-    
-print('nombre de patients sauvés (avec 2 ou moins changements:)')
-nb_change_1, nb_change_2 = results.get_patients_sauves()
+#for patient in results.donnees_test:
+#    results.diagnose_patient(patient)
+   
+print('nombre de patients sauvés (avec 2 ou moins changements):')
+#nb_change_1, nb_change_2 = results.get_patients_sauves()
 
-print(nb_change_1)
-print(nb_change_2)
+#print(nb_change_1)
+#print(nb_change_2)
 
-results.get_statistiques()
+#results.get_statistiques()
 
-#TO DO -- stabilité des resultats ? i.e pourquoi la plupart du temps on trouve 36 patients sauvé mais des fois moins ?
+print('=-=-=-=-=-=-=-=-=-=-=-=-=')
+print('TACHE #5:')
+print('arbre resultant:')
+print(arbre_advance)
+print('pourcentage de classifications correcte:')
+print(results.evaluation_model_2)
+
+
+print('=-=-=-=-=-=-=-=-=-=-=-=-=')
